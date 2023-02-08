596119e47f51bb0d743574afee24f593c2f3d74f
Jose Carlos Lozada Peralta <jose.lozada@ucb.edu.bo>
2023-02-08 16:10:23 -0400

    Version a medias

--- EjercicioD.cpp
+++ EjercicioD.cpp
@@ -22,20 +22,5 @@
 			personas[anio][mes][dia] = nombre;
 			cant++;
 		}
-		int cont=-1;
-		for (auto it = personas.rbegin(); it != personas.rend(); ++it) //ver por anio
-		{
-			for (auto it2 = personas[it->first].rbegin(); it2 != personas[it->first].rend(); ++it2)  // ver por mes
-			{
-				for (auto it3 = personas[it->first][it2->first].rbegin(); it3 != personas[it->first][it2->first].rend(); ++it3)  //ver por dia
-				{
-					cont++;
-					if (cont == 0 || cont == cant - 1) // para que muestre el primer y ultimo 
-					{
-						cout << personas[it->first][it2->first][it3->first] << endl;
-					}
-				}
-			}
-		}
 	}
 
